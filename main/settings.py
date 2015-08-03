9aaa526b2aaa6792f0e6566f80ef53d78ba72dd3
Vinay Dahiya <vinay.not.nice@gmail.com>
2015-08-04 01:08:34 +0530

    update

--- main/settings.py
+++ main/settings.py
@@ -9,8 +9,8 @@ class CeleryConfig:
     #     Queue('q1', exchange=My_Exchange),
     #     Queue('q2', exchange=My_Exchange),
     # )
-    CELERY_QUEUES = (Broadcast('broadcast_tasks', routing_key='tasks.insert_into_elastic'),
+    CELERY_QUEUES = (Broadcast(name='broadcast_tasks', queue='broadcast_q', routing_key='tasks.insert_into_elastic'),
                      Queue('q1', Exchange=Exchange(name='datacapt1', type='fanout', routing_key='tasks.insert_into_elastic1'),
                         routing_key='tasks.insert_into_elastic1'))
-    CELERY_ROUTES = {'tasks.insert_into_elastic': {'queue': 'broadcast_tasks'},
+    CELERY_ROUTES = {'tasks.insert_into_elastic': {'queue': 'broadcast_q'},
                      'tasks.insert_into_elastic1': {'queue': 'q1'}}
